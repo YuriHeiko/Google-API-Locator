fea47a391e2480c4550fba8bb591977916abf25d
YuriHeiko <yuriheiko@gmail.com>
2017-06-18 13:04:52 +0300

    Refactoring

--- src/com/heiko/placelocator/search/Impl/TargetSearchProcessor.groovy
+++ src/com/heiko/placelocator/search/Impl/TargetSearchProcessor.groovy
@@ -1,24 +1,24 @@
 package com.heiko.placelocator.search.Impl
 
 import com.heiko.placelocator.location.Places
-import com.heiko.placelocator.search.SearchHistory
+import com.heiko.placelocator.search.HistoryHolder
 import com.heiko.placelocator.search.SearchProcessor
 
-class TargetSearchProcessor implements SearchProcessor<SearchHistory<Places, Integer>> {
+class TargetSearchProcessor implements SearchProcessor<HistoryHolder<Places, Integer>> {
     private int iterationsCounter
     private final int MAX_ITERATIONS
     private final int POSSIBLE_LOCATIONS_NUMBER
 
     @Override
-    boolean isSearchFinished(SearchHistory searchHistory) {
+    boolean isSearchFinished(HistoryHolder searchHistory) {
         int size = searchHistory.getLast().size()
 
         return !(--iterationsCounter > 0 && size == 0 || size > POSSIBLE_LOCATIONS_NUMBER && iterationsCounter < MAX_ITERATIONS - 1)
     }
 
     @Override
-    int getNextFactor(SearchHistory searchHistory) {
-        // calculate and hold tendencies in SearchHistory class???
+    int getNextFactor(HistoryHolder searchHistory) {
+        // calculate and hold tendencies in HistoryHolder class???
 
         int newTendency = searchHistory.getLast().size() - searchHistory.getBeforeLast().size()
         int oldTendency = searchHistory.getBeforeLast().size() - searchHistory.get(searchHistory.size - 3)
